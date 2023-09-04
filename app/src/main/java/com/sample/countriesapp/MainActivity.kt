56807efd2a6f84091959fe6af92743b6c11cc249
Alin Babu <alinbabu2010@outlook.com>
2023-09-04 19:16:59 +0530

    Add countries screen composable to MainActivity

--- app/src/main/java/com/sample/countriesapp/MainActivity.kt
+++ app/src/main/java/com/sample/countriesapp/MainActivity.kt
@@ -3,23 +3,27 @@ package com.sample.countriesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.sample.countriesapp.presentation.CountriesScreen
+import com.sample.countriesapp.presentation.CountriesViewModel
 import com.sample.countriesapp.ui.theme.CountriesAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CountriesAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                val viewModel = hiltViewModel<CountriesViewModel>()
+                val state by viewModel.uiState.collectAsStateWithLifecycle()
+                CountriesScreen(
+                    state = state,
+                    onSelectCountry = viewModel::selectCountry,
+                    onDismissDialog = {}
+                )
             }
         }
     }
